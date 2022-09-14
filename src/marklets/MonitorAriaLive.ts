941aa2bad1701ea60787158584dd703844732532
Noah Praskins <noahpraskins@gmail.com>
2022-09-13 20:49:01 -0700

    updates and reformatting

--- src/marklets/MonitorAriaLive.ts
+++ src/marklets/MonitorAriaLive.ts
@@ -1,24 +1,35 @@
-exports = {}
-const mutationObserverCallback: MutationCallback  = (mutations, observer) => {
-console.log("mutations: ",mutations)
+function mutationObserverCallback(mutations, _observer) {
+    console.log("mutations: ", mutations)
 }
-const config: MutationObserverInit = {
-    attributes: true,
-    subtree: true,
-    childList: true
-}
-const al_attr = 'data-arialive-mutatobs-set'
-const MonitorAriaLive = ()=>{
-    const body = document.querySelector('body')
-    if (body.attributes.getNamedItem(al_attr)) {
-        console.log("Aria live mutation observers already set")
-        return
+
+//const al_attr = 'data-arialive-mutatobs-set'
+const monitoredNodes: Element[] = []
+
+function MonitorAriaLive() {
+//    const body = document.querySelector('body')
+//    if (body.attributes.getNamedItem(al_attr)) {
+//        console.log("Aria live mutation observers already set")
+//        return
+//    }
+    const config: MutationObserverInit = {
+        attributes: true,
+        subtree: true,
+        childList: true
     }
-    document.querySelectorAll('[aria-live]').forEach(x=>{
+    console.debug("Looking for aria-live regions")
+    const liveregions = document.querySelectorAll('[aria-live]')
+    liveregions && console.debug("found regions: ", liveregions)
+    liveregions.forEach(x => {
+        if (monitoredNodes.includes(x)) {
+            console.log("node already present", x)
+            return
+        }
+        monitoredNodes.push(x)
         const mo = new MutationObserver(mutationObserverCallback)
         mo.observe(x, config)
     })
-    body.setAttribute(al_attr, '')
+    console.debug("monitoredNodes: ", monitoredNodes)
+//    body.setAttribute(al_attr, '')
 
 }
 
