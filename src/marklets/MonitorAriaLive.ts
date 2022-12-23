b2a8cbe1ff5e75a282276d5d290260dc6a5de8f3
Noah Praskins <noahpraskins@gmail.com>
2022-12-23 14:06:14 -0800

    aria-live testing

--- src/marklets/MonitorAriaLive.ts
+++ src/marklets/MonitorAriaLive.ts
@@ -7,11 +7,198 @@ const config: MutationObserverInit = {
   characterDataOldValue: true,
 };
 
+const displayContainerID = "aria-live-monitor";
+const displayContainer = document.createElement("div");
+displayContainer.id = displayContainerID;
+displayContainer.style.position = "fixed";
+displayContainer.style.top = "0";
+displayContainer.style.left = "0";
+displayContainer.style.width = "100%";
+displayContainer.style.height = "100%";
+displayContainer.style.pointerEvents = "none";
+
+const styleContainer = document.createElement("style");
+const styles = `
+style {
+  display: none;
+}
+.ripple {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background: rgba(255, 0, 0, 0.5);
+  animation: ripple 2s infinite;
+}
+.arialive-result {
+  pointer-events: all;
+  background: rgba(0, 9, 0, 0.75);
+  width: 25%;
+  color: white;
+  padding-top: 1rem;
+  padding-bottom: 0.5rem;
+}
+`;
+styleContainer.innerHTML = styles;
+// document.head.appendChild(styleContainer);
+
+displayContainer.appendChild(styleContainer);
+const controlsContainer = document.createElement("div");
+controlsContainer.id = "controls-container";
+controlsContainer.style.display = "flex";
+controlsContainer.style.width = "100%";
+controlsContainer.style.justifyContent = "space-between";
+controlsContainer.style.alignItems = "center";
+controlsContainer.style.padding = "0.5rem";
+controlsContainer.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+controlsContainer.style.top = "0";
+
+const resultsContainer = document.createElement("div");
+resultsContainer.id = "results-container";
+displayContainer.appendChild(resultsContainer);
+// resultsContainer.appendChild(controlsContainer)
+displayContainer.insertAdjacentElement("afterbegin", controlsContainer);
+
+const collapseButton = document.createElement("button");
+collapseButton.innerText = "toggle aria results";
+collapseButton.style.display = "block";
+collapseButton.style.top = "0";
+collapseButton.style.left = "0";
+collapseButton.style.zIndex = "1000";
+collapseButton.style.pointerEvents = "all";
+collapseButton.addEventListener("click", () => {
+  resultsContainer.style.display = "none";
+  const oldariaExpanded = resultsContainer.getAttribute("aria-expanded");
+  const newAriaExpanded = oldariaExpanded === "true" ? "false" : "true";
+  resultsContainer.setAttribute("aria-expanded", newAriaExpanded);
+
+  resultsContainer.style.display = `${
+    newAriaExpanded === "true" ? "block" : "none"
+  }`;
+});
+controlsContainer.appendChild(collapseButton);
+
+const closeButton = document.createElement("button");
+closeButton.innerText = "closeButton";
+closeButton.style.position = "fixed";
+closeButton.style.top = "0";
+closeButton.style.right = "0";
+closeButton.style.zIndex = "1000";
+closeButton.addEventListener(
+  "click",
+  () => {
+    displayContainer.remove();
+  },
+  { once: true }
+);
+function closeOnEscape(e: KeyboardEvent): void {
+  if (e.key === "Escape") {
+    console.log("escape pressed");
+    closeButton.removeEventListener("click", () => {
+      displayContainer.remove();
+    });
+    displayContainer.remove();
+    document.removeEventListener("keyup", closeOnEscape);
+  }
+}
+
+document.addEventListener("keyup", closeOnEscape, { once: false });
+displayContainer.appendChild(closeButton);
+document.body.appendChild(displayContainer);
+
+const displaytemplate = (mutation: MutationRecord): string => {
+  const target = mutation.target as HTMLElement;
+  const attr = mutation.attributeName;
+  const value = attr ? target.getAttribute(attr) : null;
+  const oldValue = mutation.oldValue;
+  const addedNodes = Array.from(mutation.addedNodes);
+  const removedNodes = Array.from(mutation.removedNodes);
+  const textContent = target.textContent;
+  const oldTextContent = mutation.oldValue;
+  const type = mutation.type;
+  const result = `
+    <div class="arialive-result">
+        <div>type: ${type.toString()}</div>
+        <div>target: ${target.outerText}</div>
+        ${target.outerHTML ? `<div>outerHTML: ${target.outerHTML}</div>` : ""}
+        ${attr ? `<div>attribute: ${attr}</div>` : ""}
+        ${value ? `<div>value: ${value}</div>` : ""}
+        ${oldValue ? `<div>oldValue: ${oldValue}</div>` : ""}
+        ${
+          addedNodes.length
+            ? `<div>addedNodes: ${addedNodes
+                .map((x) => x.nodeName)
+                .join(", ")}</div>`
+            : ""
+        }
+        ${
+          removedNodes.length
+            ? `<div>removedNodes: ${removedNodes
+                .map((x) => x.nodeName)
+                .join(", ")}</div>`
+            : ""
+        }
+        ${textContent ? `<div>textContent: ${textContent}</div>` : ""}
+        ${oldTextContent ? `<div>oldTextContent: ${oldTextContent}</div>` : ""}
+    </div>
+    `;
+  return result;
+};
+
+function drawHighlightOverlay(element: HTMLElement): void {
+  const overlay = document.createElement("div");
+  overlay.setAttribute("aria-hidden", "true");
+  overlay.setAttribute("data-aria-live-monitor", "true");
+  overlay.style.position = "absolute";
+  overlay.style.top = "0";
+  overlay.style.left = "0";
+  overlay.style.width = "100%";
+  overlay.style.height = "100%";
+  overlay.style.zIndex = "1000";
+  // overlay.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+  overlay.style.pointerEvents = "none";
+  overlay.style.background =
+    "transparent radial-gradient(circle, transparent 1%, #47a7f5 94%) center";
+  // overlay.style.animation = 'pulse 2s infinite';
+  overlay.className = "ripple";
+  // overlay.style.boxShadow = "0 0 0 0.5rem rgba(255, 0, 0, 0.5)";
+  // overlay.style.borderRadius = "0.5rem";
+  // overlay.style.transition = "all 2s ease-in-out";
+  // element.getBoundingClientRect();
+  const rect = element.getBoundingClientRect();
+  overlay.style.top = `${rect.top}px`;
+  overlay.style.left = `${rect.left}px`;
+  overlay.style.width = `${rect.width}px`;
+  overlay.style.height = `${rect.height}px`;
+  document.body.appendChild(overlay);
+  // element.appendChild(overlay);
+  setTimeout(() => {
+    overlay.remove();
+  }, 2000);
+}
 const mutationObserverCallback: MutationCallback = (mutations, _observer) => {
+  // if displayContainer is removed, disconnect observer
+  if (!displayContainer.isConnected) {
+    console.dir(displayContainer);
+    console.log("displayContainer removed, disconnecting observer");
+    _observer.disconnect();
+    return;
+  }
+  if (resultsContainer.childElementCount > 4) {
+    while (resultsContainer.childElementCount > 4) {
+      if (resultsContainer.lastElementChild)
+        resultsContainer.removeChild(resultsContainer.lastElementChild);
+    }
+  }
   mutations.forEach((mutation) => {
+    drawHighlightOverlay(mutation.target as HTMLElement);
+    resultsContainer.insertAdjacentHTML(
+      "afterbegin",
+      displaytemplate(mutation)
+    );
     if (mutation.type === "childList") {
       console.debug("mutation: ", mutation);
-      const target = mutation.target as HTMLElement;
+      // const target = mutation.target as HTMLElement;
       const addedNodes = Array.from(mutation.addedNodes);
       const removedNodes = Array.from(mutation.removedNodes);
       console.debug("addedNodes: ", addedNodes);
--- src/scripts/web.ts
+++ src/scripts/web.ts
@@ -19,11 +19,11 @@ const AriaLiveObserver = fs.readFileSync(
 const ShowHeadings = fs.readFileSync(
   join(__dirname, "../../dist", "showHeadings.js"),
   "utf-8"
-)
+);
 const FindDuplicateIds = fs.readFileSync(
-    join(__dirname, "../../dist","FindDuplicateIds.js"),
-    "utf-8"
-)
+  join(__dirname, "../../dist", "FindDuplicateIds.js"),
+  "utf-8"
+);
 const root = document.querySelector("#root") as HTMLDivElement;
 const makeLink = (x, name: string) => {
   const anchorElement = document.createElement("a");
@@ -36,5 +36,27 @@ makeLink(fcs, "fcs");
 makeLink(ForceFocusOutline, "ForceFocusOutline");
 makeLink(TextSpacing, "TextSpacing");
 makeLink(AriaLiveObserver, "AriaLiveObserver");
-makeLink(ShowHeadings, "ShowHeadings")
-makeLink(FindDuplicateIds, "FindDuplicateIds")
+makeLink(ShowHeadings, "ShowHeadings");
+makeLink(FindDuplicateIds, "FindDuplicateIds");
+
+// random text generator
+function generateRandomText() {
+  const randomText: string[] = [];
+  const randomLength = Math.floor(Math.random() * 10) + 1;
+  for (let i = 0; i < randomLength; i++) {
+    randomText.push(Math.random().toString(36).substring(2, 15));
+  }
+  return randomText.join(" ");
+}
+
+setInterval(() => {
+  try {
+    (
+      document.getElementById("live-region") as HTMLSpanElement
+    ).innerText = `poop ${generateRandomText()}`;
+  } catch (e) {
+    console.error(e);
+    clearInterval(this);
+  }
+}, 6000);
+document.getElementById("live-region") as HTMLSpanElement;
