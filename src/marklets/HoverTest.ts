80793b229bb4a6f1d43d66ec7ce4699be12e419a
Noah Praskins <noahpraskins@gmail.com>
2023-03-01 14:39:04 -0800

    hovertesting

--- src/marklets/HoverTest.ts
+++ src/marklets/HoverTest.ts
@@ -1,14 +1,11 @@
 import { createPointerSelectorListener } from "../utils/pointerSelector";
 import { getDescription, getName } from "aria-api";
 
-function LogAriaAttributes(e: MouseEvent): boolean {
-  const target = e.target;
-  if (!(target instanceof HTMLElement)) {
-    console.debug("target is not an HTMLElement");
-    return false;
-  }
-  console.debug("name: ", getName(target));
-  console.debug("description ", getDescription(target));
+function LogAriaAttributes(target: HTMLElement): boolean {
+
+  console.log("target: ", target);
+  console.log("name: ", getName(target));
+  console.log("description ", getDescription(target));
   return true;
 }
 createPointerSelectorListener(undefined, LogAriaAttributes);
--- src/utils/pointerSelector.ts
+++ src/utils/pointerSelector.ts
@@ -1,109 +1,153 @@
+// noinspection SpellCheckingInspection
 let _pointerSelector: HTMLElement;
-let _latestCoordinates: { x: number; y: number } = { x: 0, y: 0 };
+let _latestCoordinates: { x: number; y: number } = {x: 0, y: 0};
 let _debounceTimer: string | number | NodeJS.Timeout | undefined;
 // This is a helper function to help you find the element you want to click on.
 // It will create a red box around the element you hover over.
 // You can pass a click handler that will be called when you click on the element.
 // If you return true from the click handler, the pointer selector element will be removed.
 function createPointerSelector(
-  clickHandler?: (e: MouseEvent) => boolean
+    clickHandler?: (e: HTMLElement) => boolean
 ): boolean {
-  if (_pointerSelector) {
-    console.warn("Pointer selector already exists");
-    return false;
-  }
-  function clickHandlerResolver(click: MouseEvent): void {
-    if (clickHandler?.(click)) {
-      document
-        .getElementById("phlffobkmklt")
-        ?.removeEventListener("click", clickHandlerResolver);
-      document
-        .getElementById("phlffobkmklt")
-        ?.removeEventListener("mouseover", pointerSelectorMouseEventForwarder);
-      document.getElementById("phlffobkmklt")?.remove();
+    if (_pointerSelector) {
+        console.warn("Pointer selector already exists");
+        return false;
     }
-  }
-  _pointerSelector = document.createElement("div");
-  _pointerSelector.className = "phlffobkmklt";
-  _pointerSelector.style.position = "absolute";
-  _pointerSelector.id = "phlffobkmklt";
-  _pointerSelector.style.zIndex = "100000";
-  _pointerSelector.style.border = "2px solid red";
-  _pointerSelector.style.outline = "2px solid orange";
-  _pointerSelector.style.outlineOffset = "2px";
-  _pointerSelector.style.opacity = "0.5";
-  if (typeof clickHandler === "function") {
-    _pointerSelector.style.pointerEvents = "auto";
-    _pointerSelector.addEventListener("click", clickHandlerResolver, {
-      passive: true,
-      once: false,
-    });
-    _pointerSelector.addEventListener(
-      "mouseover",
-      pointerSelectorMouseEventForwarder,
-      {
-        passive: false,
-        once: false,
-        capture: true,
-      }
-    );
-  } else {
-    _pointerSelector.style.pointerEvents = "none";
-  }
-  _pointerSelector = document.body.appendChild(_pointerSelector);
-  return true;
+
+    function clickHandlerResolver(_: MouseEvent): void {
+        const t = getTargetFromCachedCoords();
+        console.log("clickHandlerResolver target", t)
+
+        if (clickHandler?.(t) === true) {
+            document
+                .getElementById("phlffobkmklt")
+                ?.removeEventListener("click", clickHandlerResolver);
+            document
+                .getElementById("phlffobkmklt")
+                ?.removeEventListener("mouseover", pointerSelectorMouseEventForwarder);
+            document.getElementById("phlffobkmklt")?.remove();
+        }
+    }
+
+    _pointerSelector = document.createElement("div");
+    _pointerSelector.className = "phlffobkmklt";
+    _pointerSelector.style.position = "absolute";
+    _pointerSelector.id = "phlffobkmklt";
+    _pointerSelector.style.zIndex = "999999999999";
+    _pointerSelector.style.border = "2px solid red";
+    _pointerSelector.style.outline = "2px solid orange";
+    _pointerSelector.style.outlineOffset = "2px";
+    _pointerSelector.style.opacity = "0.5";
+    if (typeof clickHandler === "function") {
+        _pointerSelector.style.pointerEvents = "auto";
+        _pointerSelector.addEventListener("click", clickHandlerResolver, {
+            passive: true,
+            once: false,
+        });
+        _pointerSelector.addEventListener("mousemove", pointerSelectorMouseEventForwarder, {
+            passive: false,
+            once: false,
+            capture: true,
+        })
+        _pointerSelector.addEventListener(
+            "mouseover",
+            pointerSelectorMouseEventForwarder,
+            {
+                passive: false,
+                once: false,
+                capture: true,
+            }
+        );
+    } else {
+        _pointerSelector.style.pointerEvents = "none";
+    }
+    _pointerSelector = document.body.appendChild(_pointerSelector);
+    return true;
 }
-const getTargetFromCachedCoords: () => HTMLElement = () =>
-  document.elementFromPoint(
-    _latestCoordinates.x,
-    _latestCoordinates.y
-  ) as HTMLElement;
+
+const getTargetFromCachedCoords: () => HTMLElement = () => {
+    _pointerSelector.style.display = "none";
+    const t =  document.elementFromPoint(
+        _latestCoordinates.x,
+        _latestCoordinates.y
+    ) as HTMLElement;
+    _pointerSelector.style.display = "block";
+    return t
+
+};
+
+function forwardEvent(e: MouseEvent): void {
+    // _pointerSelector.style.display = "none";
+    const lowerEl = getTargetFromCachedCoords();
+
+    let eventType = e.type;
+    switch (e.type) {
+        case "click":
+        case "mouseover":
+        case "mousemove":
+            eventType = "mouseover";
+            break;
+        default:
+            console.log("hit default with event, ", e)
+            return
+
+    }
+
+    lowerEl?.dispatchEvent(
+        new MouseEvent(eventType, {
+            bubbles: true,
+
+        })
+    );
+
+    _debounceTimer = undefined;
+}
 
 function pointerSelectorMouseEventForwarder(e: MouseEvent): void {
-  // _pointerSelector.style.pointerEvents = "none";
+    // _pointerSelector.style.pointerEvents = "none";
 
-  e.stopPropagation();
-  _latestCoordinates = { x: e.pageX, y: e.pageY };
-  if (_debounceTimer) {
-    clearTimeout(_debounceTimer);
-  }
-  _debounceTimer = setTimeout(() => {
-    _pointerSelector.style.display = "none";
-    const lowerEl = getTargetFromCachedCoords();
-    lowerEl?.dispatchEvent(
-      new MouseEvent(e.type, {
-        bubbles: true,
-      })
-    );
-    console.debug("lower el: ", lowerEl);
-    _debounceTimer = undefined;
-    _pointerSelector.style.display = "block";
-  }, 500);
+    e.stopPropagation();
+    // forwardEvent(e)
+    _latestCoordinates = {x: e.pageX, y: e.pageY};
+    if (_debounceTimer) {
+        clearTimeout(_debounceTimer);
+    }
+    if (e.type === "click") {
+        forwardEvent(e)
+    } else {
+        _debounceTimer = setTimeout(() => {
+            forwardEvent(e)
+        }, 100);
 
-  // _pointerSelector.style.pointerEvents = "auto";
+    }
+
+    // _pointerSelector.style.pointerEvents = "auto";
 }
+
 function adjustPointerSelector(target: HTMLElement): void {
-  const { x: x1, y: y1, height, width } = target.getBoundingClientRect();
-  // const y2 = y1 + height;
-  // const x2 = x1 + width;
-  const pointerSelector = document.getElementById(
-    "phlffobkmklt"
-  ) as HTMLElement;
-  pointerSelector.style.top = `${y1}px`;
-  pointerSelector.style.left = `${x1}px`;
-  pointerSelector.style.width = `${width}px`;
-  pointerSelector.style.height = `${height}px`;
+
+    const {x: x1, y: y1, height, width} = target.getBoundingClientRect();
+    // const y2 = y1 + height;
+    // const x2 = x1 + width;
+    // const pointerSelector = document.getElementById(
+    //   "phlffobkmklt"
+    // ) as HTMLElement;
+    _pointerSelector.style.top = `${y1 + window.scrollY}px`;
+    _pointerSelector.style.left = `${x1 + window.scrollX}px`;
+    _pointerSelector.style.width = `${width}px`;
+    _pointerSelector.style.height = `${height}px`;
 }
+
 function mouseoverHandler(
-  e: MouseEvent,
-  callback?: (me: MouseEvent) => void
+    e: MouseEvent,
+    callback?: (me: HTMLElement) => void
 ): void {
-  const target = e.target as HTMLElement;
-  adjustPointerSelector(target);
-  if (callback) {
-    callback(e);
-  }
-  console.debug(target);
+    // console.log("mouseoverHandler called", e)
+
+    const target = e.target as HTMLElement;
+    // if (target === document.firstElementChild) return;
+    adjustPointerSelector(target);
+    callback?.(target);
 }
 
 // Creates a pointer selector that will create a red box around the element you hover over.
@@ -112,19 +156,22 @@ function mouseoverHandler(
 //
 // You can also pass a hover handler that will be called when you hover over an element.
 function createPointerSelectorListener(
-  hoverCallback?: (t: MouseEvent) => void,
-  clickCallback?: (t: MouseEvent) => boolean
+    hoverCallback?: (t: HTMLElement) => void,
+    clickCallback?: (t: HTMLElement) => boolean
 ): void {
-  if (!createPointerSelector(clickCallback)) return;
-  document.addEventListener(
-    "mouseover",
-    (e) => {
-      mouseoverHandler(e, hoverCallback);
-    },
-    {
-      passive: true,
-      once: false,
-    }
-  );
+    if (!createPointerSelector(clickCallback)) return;
+
+
+    document.addEventListener(
+        "mouseover",
+        (e) => {
+            mouseoverHandler(e, hoverCallback);
+        },
+        {
+            passive: true,
+            once: false,
+        }
+    );
 }
-export { createPointerSelectorListener };
+
+export {createPointerSelectorListener};
